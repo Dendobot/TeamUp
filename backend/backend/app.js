d792c665ae69d6caf9d53325180d4e19f907a588
Dendobot <87963076+Dendobot@users.noreply.github.com>
2022-09-08 12:45:28 +1000

    [US1.1] (<Your Name>, <Name>) Create landing page

---
+++ backend/backend/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config();
+const PORT = process.env.PORT || 3500;
+const verifyJWT = require("./config/verifyJWT");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const corsOptions = require('./config/corsOptions');
+
+//mongoose
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('connected,,'))
+  .catch((err) => console.log(err));
+
+
+
+//BodyParser
+app.use(bodyParser.json()); //Handles JSON requests
+app.use(bodyParser.urlencoded({ extended: false })); //Handles normal post requests
+
+//middleware for cookie
+app.use(cookieParser());
+
+
+//cors for listenning from different port
+app.use(cors(corsOptions));
+
+
+//Routes
+app.use('/users', require('./routes/users'));
+app.use(verifyJWT);
+app.use('/recipe', require('./routes/recipe'));
+//verify token before give access to route
+
+app.use('/admin', require('./routes/admin'));
+
+//verify token first
+
+//app.use('/dashboard', require('./routes/dashboard'));
+
+app.listen(PORT, function () {
+  console.log("server is listening on port: " + PORT);
+});
---
+++ backend/backend/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  recipes: {
+    type: [String],
+    required: false
+  },
+  refreshToken: {
+    type: String,
+    required: false
+  }
+});
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
