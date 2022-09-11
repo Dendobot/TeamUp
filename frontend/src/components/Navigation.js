3b3f09168d67061a65c80d156b4435d828737194
RahafAlmadani <almadanirahaf@gmail.com>
2022-09-11 14:28:59 +1000

    [US1.1] (Rahaf) Make logo clickable

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -5,7 +5,7 @@ function Navigation() {
   return (
     <nav class="navbar navbar-expand-lg navbar-light ">
       <div className="left">
-        <h1>Cheffie</h1>
+        <h1 > <a href="/landingPage">Cheffie </a> </h1>
         </div>
           <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNav" 
           aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
