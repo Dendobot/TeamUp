6d988f9c032e751d0cc8dbfb549c297aaa58a476
RahafAlmadani <almadanirahaf@gmail.com>
2022-09-11 14:06:25 +1000

    [US1.1] (rahaf) delete app.css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home, About, SignIn, Footer , SignUp, AddRecipe} from "./components";
 
