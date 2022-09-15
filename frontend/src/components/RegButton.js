8197bb2b18d456a89894fe57b3c5499d108101bd
RahafAlmadani <almadanirahaf@gmail.com>
2022-09-15 12:51:05 +1000

    [US1.1] (rahaf) Fixing bugs

--- frontend/src/components/RegButton.js
+++ frontend/src/components/RegButton.js
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+
 import { Button } from "@mui/material";
 import React from "react";
 
@@ -6,7 +6,7 @@ export class RegButton extends React.Component {
   render() {
     return (
       <>
-        <Button variant="contained" size = "large">{this.props.text}</Button>   
+        <Button variant="contained" size = "large"> {this.props.text} </Button>   
       </>
     );
   }
