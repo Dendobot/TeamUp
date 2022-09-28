46a1a0294d566686d13cdcb93781b200f75c715f
ramyaiyer2001 <ramyaiyer2001@gmail.com>
2022-09-28 17:27:47 +1000

    Created Dynamic Ingredient List

--- frontend/src/components/UploadAndDisplayImage.js
+++ frontend/src/components/UploadAndDisplayImage.js
@@ -1,38 +1,148 @@
-import { TextField } from "@mui/material";
-import React, { useState } from "react";
-import { Grid, Button} from "@mui/material";
+import { TextField, InputAdornment } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Grid,
+  IconButton,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+} from "@mui/material";
+
+import AddCircleRoundedIcon from "@mui/icons-material/AddCircleRounded";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const UploadAndDisplayImage = () => {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [rows, setRows] = useState([
+    {
+      id: 1,
+      item: "",
+    },
+  ]);
 
-  /*<input
-          className="uploade-photo"
-          type="file"
-          name="myImage"
-          onChange={(event) => {
-            setSelectedImage(event.target.files[0]);
-          }}
-        />
-        {selectedImage && (
-          <div>
-            <br />
-            <div
-              onClick={() => setSelectedImage(null)}
-              className="uploade-photo"
+  const generateRow = () => {
+    return {
+      id: rows.length + 1,
+      item: "",
+    };
+  };
+
+  const handleDeleteRow = (event, id) => {
+    setRows((rows) => {
+      rows[id - 1].item = "";
+      for (let i = id - 1; i < rows.length; i++) {
+        rows[i].id--;
+      }
+    });
+
+    setRows((prevRows) => {
+      return [...rows.slice(0, id - 1), ...rows.slice(id)];
+    });
+  };
+
+  const handleChange = (e, id) => {
+    let value = e.target.value;
+
+    if (id === rows.length) {
+      if (value !== "") {
+        setRows((prevRows) => [...prevRows, generateRow()]);
+      }
+    } else if (id + 1 === rows.length) {
+      if (value === "") {
+        handleDeleteRow(e, id + 1);
+      }
+    }
+    setRows((prevRows) => {
+      return prevRows.map((row, index) =>
+        index === id - 1 ? { ...row, size: value } : row
+      );
+    });
+  };
+
+  /*const handleSave = () => {
+    let arr = [];
+    if (rows[rows.length - 1].item === "") {
+      arr = rows.slice(0, -1);
+    } else {
+      arr = rows;
+    }
+
+    let items = [];
+    for (let i = 0; i < arr.length; i++) {
+      items.push(arr[i].item);
+    }
+
+    window.sessionStorage.setItem("list", JSON.stringify(items));
+  };*/
+
+  const generateTable = () => {
+    return (
+      <Table>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
-              Remove
-            </div>
-            <br />
-            <br />
-            <img
-              className="photo-box"
-              alt="not fount"
-              width={"250px"}
-              src={URL.createObjectURL(selectedImage)}
-            />
-          </div>
-        )}*/
+              <TableCell component="th" scope="row">
+                <TextField
+                  label = "Add Ingredient"
+                  className="bg-color"
+                  id="outlined-basic"
+                  variant="outlined"
+                  value={row.size}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <IconButton onClick={(e) => handleChange(e, row.id)} aria-label="add to ingredient list">
+                          <AddCircleRoundedIcon color="primary" />
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </TableCell>
+              <TableCell component="th" scope="row">
+                {row.id === rows.length ? (
+                  <></>
+                ) : (
+                  <IconButton onClick={(e) => handleDeleteRow(e, row.id)}>
+                    <DeleteIcon />
+                  </IconButton>
+                )}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    );
+  };
 
+  const [tableData, setTableData] = useState(generateTable());
+
+  useEffect(() => {
+    setTableData(generateTable());
+  }, [rows]);
+
+  useEffect(() => {
+    if (window.sessionStorage.getItem("list")) {
+      let sizes_arr = window.sessionStorage.getItem("list").split(",");
+      sizes_arr.push("");
+      let rows_arr = [];
+      for (let i = 0; i < sizes_arr.length; i++) {
+        rows_arr.push({
+          id: i + 1,
+          size: sizes_arr[i],
+        });
+      }
+
+      setRows(rows_arr);
+      setTableData(generateTable());
+    }
+  }, []);
   return (
     <div className="secondary-color">
       <Grid container rowSpacing={1} columnSpacing={{ xs: 0, sm: 0, md: 0 }}>
@@ -87,23 +197,14 @@ const UploadAndDisplayImage = () => {
               size="small"
               multiline
               minRows={"7"}
-              sx={{ width: "346px", marginBottom: "20px"}}
+              sx={{ width: "346px", marginBottom: "20px" }}
             />
           </div>
         </Grid>
         <Grid className="setGridMargin" xs={4}>
           <div className="left">
             <p className="recipeTitle">Ingredients</p>
-            <TextField
-              className="bg-color"
-              id="outlined-basic"
-              label=" "
-              variant="outlined"
-              size="small"
-              multiline
-              minRows={"7"}
-              sx={{ width: "346px", marginBottom: "20px"}}
-            />
+            <TableContainer>{tableData}</TableContainer>
           </div>
         </Grid>
         <Grid className="setGridMargin" xs={3.5}>
@@ -119,10 +220,14 @@ const UploadAndDisplayImage = () => {
               minRows={"19"}
               sx={{ width: "346px", marginBottom: "20px" }}
             />
-              <Button variant="contained" size="small" type="submit"
-              sx={{ marginLeft: "190px"}}>
-                Save Changes
-              </Button>
+            <Button
+              variant="contained"
+              size="small"
+              type="submit"
+              sx={{ marginLeft: "190px" }}
+            >
+              Save Changes
+            </Button>
           </div>
         </Grid>
       </Grid>
