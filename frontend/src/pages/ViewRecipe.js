fe07034dc9a5ca5b9e234076db5a9655d456b90a
Rachel <Rachel@Chans-MacBook-Pro.local>
2022-10-01 16:30:42 +1000

    [US5] (Rachel) Fixed font style

--- frontend/src/pages/ViewRecipe.js
+++ frontend/src/pages/ViewRecipe.js
@@ -18,9 +18,10 @@ function ViewRecipe() {
         <h5 className="ingredients">Ingredients</h5>
         <h5 className="steps">Steps</h5>
         <h5 className="ingredients" style={{position: 'absolute',width: '125px',height: '27px', left: '294px',top: '517px'}}>Cooking Time (in minutes): 5</h5>
-        <h5 style={{color: '#0671B7', position: 'absolute', width: '133px', height: '22px', left: '103px',top: '571px'}}> Notes </h5>
+        <h5 className="ingredients" style={{color: '#0671B7', position: 'absolute', width: '133px', height: '22px', left: '103px',top: '571px'}}> Notes </h5>
         <div className ="white-box" style={{position: 'absolute', width: '346px', height:'289px', left: '88px', top:'219px'}}></div>
         <div className ="white-box"></div>
+        
         <div className="steps-box"></div>
       </div>
     );
