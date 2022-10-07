2f5558d34acc24fbb296b28eb9eb1e9037b2c7b5
Joanna Chin <chinjm@student.unimelb.edu.au>
2022-10-07 16:12:57 +1100

    [BUG4] (Joanna) keep at bottom of page after all other content

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Landing, About, SignIn, Footer, SignUp, AddRecipe, Recipes } from "./components";
+import {
+  Landing,
+  About,
+  SignIn,
+  Footer,
+  SignUp,
+  AddRecipe,
+  Recipes,
+} from "./components";
 import RequiredAuth from "./components/RequiredAuth";
 
-
-function App () {
+function App() {
   return (
-    <div className="App">
+    <div>
       <Router>
         <Routes>
           <Route path="/" element={<Landing />}></Route>
@@ -15,19 +22,17 @@ function App () {
           <Route path="/signin" element={<SignIn />}></Route>
           <Route path="/signup" element={<SignUp />}></Route>
 
-
-
           {/*protected routes*/}
-          <Route element={<RequiredAuth />} >
+          <Route element={<RequiredAuth />}>
             <Route path="/addRecipe" element={<AddRecipe />}></Route>
             <Route path="/Recipes" element={<Recipes />}></Route>
           </Route>
 
           {/*catch all*/}
-          <Route path='*' element={<Landing />} />
+          <Route path="*" element={<Landing />} />
         </Routes>
-        <Footer />
       </Router>
+      <Footer />
     </div>
   );
 }
--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,28 +1,31 @@
 import React from "react";
-import BottomBar from "../components/BottomBar"
+import BottomBar from "../components/BottomBar";
 import { Button } from "@mui/material";
 import Navigation from "../components/Navigation";
 function Landing() {
   return (
-    <div>
+    <div className="landing-container">
       <Navigation />
-    <div className="home">
-      <div className="grid-container ">
-      <div className="landingPageText">
-        <div className="lets-get-text">Let’s get</div>
-        <div className="cooking">cooking!</div>
-        <h3 className="EasilyLandingPage">Easily store recipes to</h3>
-        <h3 className="EasilyLandingPage">recreate at any time with</h3>
-        <h3 className="EasilyLandingPage">Foodie</h3>
-        <a href="../signup">
-        <Button variant="contained" size="large" >
-          Sign me up!
-                      </Button></a>
+      <div className="home">
+        <div className="grid-container ">
+          <div className="landingPageText">
+            <div className="lets-get-text">Let’s get</div>
+            <div className="cooking">cooking!</div>
+            <h3 className="EasilyLandingPage">Easily store recipes to</h3>
+            <h3 className="EasilyLandingPage">recreate at any time with</h3>
+            <h3 className="EasilyLandingPage">Foodie</h3>
+            <a href="../signup">
+              <Button variant="contained" size="large">
+                Sign me up!
+              </Button>
+            </a>
+          </div>
+          <img
+            src={`https://file.rendit.io/n/rZRVDlqtBdNumaJBs8iy.png`}
+            alt="landing page"
+          />
+        </div>
       </div>
-      <img src={`https://file.rendit.io/n/rZRVDlqtBdNumaJBs8iy.png`}alt="landing page"/>
-    </div>
-    <BottomBar></BottomBar>
-    </div>
     </div>
   );
 }
