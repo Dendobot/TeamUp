a32e372384f9e5b05b87f8a45d0c776265f4e0af
ramyaiyer2001 <ramyaiyer2001@gmail.com>
2022-10-13 18:24:33 +1100

    [BUG] Fixed import bug

--- frontend/src/pages/EditRecipe.js
+++ frontend/src/pages/EditRecipe.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react";
+import React, {useEffect, useState } from "react";
 import Navigation from "../components/Navigation";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import { useParams, useNavigate } from "react-router-dom";
