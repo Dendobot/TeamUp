1af17306faaa15e4ec791b60a1f8066bc23f7789
ramyaiyer2001 <ramyaiyer2001@gmail.com>
2022-10-24 00:11:56 +1100

    [BUG](Ramya) The collapsed navbar is now working

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -2,9 +2,9 @@ import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import SignOutButton from "./SignOutButton";
 import useAuth from "../hooks/useAuth";
+import { Divider, Menu, MenuItem } from "@mui/material";
 
-
-function Navigation () {
+function Navigation() {
   //this signout button will only appear if you are logged in
   var loginButton = <></>;
   var home = <></>;
@@ -13,11 +13,33 @@ function Navigation () {
   var signout = <></>;
   var myRecipes = <></>;
   var cheffie = <></>;
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleListKeyDown = (event) => {
+    if (event.key === "Tab") {
+      setAnchorEl(null);
+    } else if (event.key === "Escape") {
+      anchorEl.focus();
+      setAnchorEl(null);
+    }
+  };
+
   const location = useLocation();
   const { auth } = useAuth();
   if (auth?.user) {
     console.log("location: " + location.pathname);
-    if (!location.pathname.includes("/editRecipe") && !location.pathname.includes("/addRecipe")) {
+    if (
+      !location.pathname.includes("/editRecipe") &&
+      !location.pathname.includes("/addRecipe")
+    ) {
       loginButton = <SignOutButton />;
     }
 
@@ -58,8 +80,9 @@ function Navigation () {
 
   return (
     <div
-      className={`row justify-content-center ${auth?.user && "light-blue-background"
-        }`}
+      className={`row justify-content-center ${
+        auth?.user && "light-blue-background"
+      }`}
     >
       <nav class="navbar navbar-expand-lg navbar-light col-sm-12 col-md-10 col-xl-8">
         <h1>{cheffie}</h1>
@@ -71,9 +94,36 @@ function Navigation () {
           aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
+          onClick={handleClick}
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
+        {Boolean(auth?.user) === false ? (
+          <Menu
+            id="demo-positioned-menu"
+            aria-labelledby="demo-positioned-button"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>{home}</MenuItem>
+            <MenuItem onClick={handleClose}>{signin}</MenuItem>
+            <MenuItem onClick={handleClose}>{signout}</MenuItem>
+          </Menu>
+        ) : (
+          <Menu
+            id="menu-appbar"
+            aria-labelledby="demo-positioned-button"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>{myRecipes}</MenuItem>
+            <MenuItem onClick={handleClose}>{loginButton}</MenuItem>
+          </Menu>
+        )}
+
         <div className="navbar-collapse collapse" id="navbarNav">
           <ul className="navbar-nav ml-auto">
             <li className="nav-item navBarLinks">{home}</li>
